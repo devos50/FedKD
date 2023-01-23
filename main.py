a755d2c652cef4f9f44274972c53803b81394ff4
Martijn de Vos <martijn.devos@epfl.ch>
2023-01-23 10:28:50 +0100

    Fixed experiments with N > 20

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     publicdata = 'cifar100' if args.dataset=='cifar10' else 'imagenet'
     args.N_class = 10 if args.dataset=='cifar10' else 100
     priv_data, _, test_dataset, public_dataset, distill_loader = data_cifar.dirichlet_datasplit(
-        args, privtype=args.dataset, publictype=publicdata, N_parties=20, online=not args.oneshot, public_percent=args.public_percent)
+        args, privtype=args.dataset, publictype=publicdata, N_parties=args.N_parties, online=not args.oneshot, public_percent=args.public_percent)
     test_loader = DataLoader(
         dataset=test_dataset, batch_size=args.batchsize, shuffle=False, num_workers=args.num_workers, sampler=None)
     
